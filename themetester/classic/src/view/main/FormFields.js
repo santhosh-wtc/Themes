82cab00884229b1aba4c3dbe64a4cf84503c93c5
Debabrata Sarker <debabrata.sarker@walkingtree.in>
2016-12-06 19:33:39 +0530

    Committing changes for the combo box (not the drop-dowm a/c to the theme pics)

--- themetester/classic/src/view/main/FormFields.js
+++ themetester/classic/src/view/main/FormFields.js
@@ -2,6 +2,34 @@ Ext.define('ThemeTester.view.main.FormFields', {
     extend: 'Ext.container.Container',
     xtype: 'formfields',
 
-    html : 'Form Field'
+    html : 'Form Field',
+    padding: 50,
+
+     items:[
+      {
+            xtype: 'combobox',
+            fieldLabel: 'Test Combo',
+            emptyText: 'Select One',
+            store: new Ext.data.SimpleStore({
+                    data: [
+                        [1, 'List Item 1'],
+                        [2, 'List Item 2'],
+                        [3, 'List Item 3'],
+                        [4, 'List Item 4']                    
+                    ],
+                    id: 0,
+                    fields: ['value', 'text']
+                }),
+                valueField: 'value',
+                displayField: 'text',
+                triggerAction: 'all',
+                editable: false
+            },
+            {
+            	xtype: 'textfield',
+            	fieldLabel: 'Text Box',
+            	emptyText: 'Input'
+            }
+     ]
 
 });
