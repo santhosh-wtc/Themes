a913238e48a4de8c3a5e617dc350c67f13cda3c5
walkingtree-PC\walkingtree <santhosh.kumar@walkingtree.tech>
2016-12-07 12:08:58 +0530

    Lightweight Combobox css added

--- themetester/classic/src/view/main/FormFields.js
+++ themetester/classic/src/view/main/FormFields.js
@@ -2,13 +2,11 @@ Ext.define('ThemeTester.view.main.FormFields', {
     extend: 'Ext.container.Container',
     xtype: 'formfields',
 
-    html : 'Form Field',
     padding: 50,
 
-     items:[
-      {
+     items:[{
             xtype: 'combobox',
-            fieldLabel: 'Test Combo',
+            fieldLabel: 'Combo Box',
             emptyText: 'Select One',
             store: new Ext.data.SimpleStore({
                     data: [
@@ -29,6 +27,46 @@ Ext.define('ThemeTester.view.main.FormFields', {
             	xtype: 'textfield',
             	fieldLabel: 'Text Box',
             	emptyText: 'Input'
+            },
+            {
+            xtype: 'combobox',
+            ui : 'dropdown',
+            fieldLabel: 'Drop Down',
+            emptyText: 'Select One',
+            store: new Ext.data.SimpleStore({
+                    data: [
+                        [1, 'List Item 1'],
+                        [2, 'List Item 2'],
+                        [3, 'List Item 3'],
+                        [4, 'List Item 4']                    
+                    ],
+                    id: 0,
+                    fields: ['value', 'text']
+                }),
+                valueField: 'value',
+                displayField: 'text',
+                triggerAction: 'all',
+                editable: false
+            },
+            {
+            xtype: 'combobox',
+            ui : 'lightdropdown',
+            fieldLabel: 'Light Drop Down',
+            emptyText: 'Select One',
+            store: new Ext.data.SimpleStore({
+                    data: [
+                        [1, 'List Item 1'],
+                        [2, 'List Item 2'],
+                        [3, 'List Item 3'],
+                        [4, 'List Item 4']                    
+                    ],
+                    id: 0,
+                    fields: ['value', 'text']
+                }),
+                valueField: 'value',
+                displayField: 'text',
+                triggerAction: 'all',
+                editable: false
             }
      ]
 
